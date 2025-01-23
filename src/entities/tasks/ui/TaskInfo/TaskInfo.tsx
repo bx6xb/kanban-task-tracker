222d6b0643e6c10a2d7be5a122bb263ea1a7c768
Yan Turnt <bx6xbchosen1@gmail.com>
2025-01-23 13:31:04 +0500

    fix: fix expired date color display

--- src/entities/tasks/ui/TaskInfo/TaskInfo.tsx
+++ src/entities/tasks/ui/TaskInfo/TaskInfo.tsx
@@ -1,10 +1,15 @@
-import clsx from "clsx";
-import s from "./TaskInfo.module.scss";
-import { removeTask } from "../../model";
-import { TaskCardProps } from "../TaskCard";
-import { formatDate, Icon, isDateExpired, useAppDispatch } from "../../../../shared";
+import clsx from 'clsx'
+import s from './TaskInfo.module.scss'
+import { removeTask } from '../../model'
+import { TaskCardProps } from '../TaskCard'
+import {
+  formatDate,
+  Icon,
+  isDateExpired,
+  useAppDispatch,
+} from '../../../../shared'
 
-type Props = { toggleEditMode(): void } & TaskCardProps;
+type Props = { toggleEditMode(): void } & TaskCardProps
 
 export const TaskInfo = ({
   endDay,
@@ -15,46 +20,50 @@ export const TaskInfo = ({
   toggleEditMode,
   type,
 }: Props) => {
-  const dispatch = useAppDispatch();
+  const dispatch = useAppDispatch()
 
-  const removeTaskCallback = () => dispatch(removeTask(id));
+  const removeTaskCallback = () => dispatch(removeTask(id))
+
+  if (formatDate(endDay) === '20.11.2023') {
+    console.log(formatDate(endDay), isDateExpired(endDay) && type !== 'done')
+  }
 
   return (
     <>
-      <div className={"rowContainer"}>
-        <div className={"row"}>
+      <div className={'rowContainer'}>
+        <div className={'row'}>
           Начало:
           <span>{formatDate(startDay)}</span>
         </div>
 
-        <div className={"row"}>
+        <div className={'row'}>
           Окончание:
           <span
             className={clsx(
-              isDateExpired(endDay) && type !== "done" && s.expired,
+              isDateExpired(endDay) && type !== 'done' && 'expired'
             )}
           >
             {formatDate(endDay)}
           </span>
         </div>
 
-        <div className={"row"}>
+        <div className={'row'}>
           Описание:
           <span className={s.text}>{text}</span>
         </div>
       </div>
 
-      <div className={"options"}>
+      <div className={'options'}>
         {isEditable && (
-          <button onClick={toggleEditMode} type={"button"}>
-            <Icon className={s.edit} height={18} id={"edit"} width={18} />
+          <button onClick={toggleEditMode} type={'button'}>
+            <Icon className={s.edit} height={18} id={'edit'} width={18} />
           </button>
         )}
 
-        <button onClick={removeTaskCallback} type={"button"}>
-          <Icon className={s.trash} height={18} id={"trash"} width={18} />
+        <button onClick={removeTaskCallback} type={'button'}>
+          <Icon className={s.trash} height={18} id={'trash'} width={18} />
         </button>
       </div>
     </>
-  );
-};
+  )
+}
